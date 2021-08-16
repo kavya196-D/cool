3193b04187d7cc64595eb055f133445b7e103e03
kavya196-D <85491652+kavya196-D@users.noreply.github.com>
2021-08-16 16:50:57 +0530

    ORDER FOOD ITEM.c

---
+++ Order food item .c
@@ -0,0 +1,35 @@
+#include<stdio.h> 
+main() 
+{ 
+
+        printf("enter the item number:");
+        int choice=0;
+        scanf("%d",&choice);
+        switch(choice)
+        {
+             case 1:
+                   printf("food item:pizza");
+                   printf("Price:210"); 
+                   break;                         
+             case 2:
+                   printf("food item:burger");
+                   printf("Price:180"); 
+                   break;
+             case 3:
+                   printf("fooditem:pasta");                printf("\nPrice:179");
+                   break;
+             case 4:
+                   printf("food item:french ")    
+                   printf("\nPrice:210");
+                   break;
+             case 5:
+                   printf("food item:sandwich");
+                   printf("\nPrice:100");
+                   break;
+             default:printf("Invalid syntex");
+              }
+}
+OUTPUT:
+   enter the item number:3
+   Food item:pasta
+   Price:180
