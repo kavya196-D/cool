cb2051c241aea5d1111d1ba9c7916521bba707d9
kavya196-D <85491652+kavya196-D@users.noreply.github.com>
2021-08-16 15:55:59 +0530

    Print the pattern.c

---
+++ Print the pattern .c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()  {
+
+   int n,i,j;
+
+   printf("Enter number of rows - ");
+
+   scanf("%d",&n);
+
+   for(i=1;i<=n;i++)   {
+
+       for(j=0;j<2*i;j++)
+
+           printf("%d",j%2);
+
+       printf("\n");
+
+   }
+
+   return 0;
+
+}
+
+OUTPUT::
+
+Enter number of rows -6
+
+                01
+                0101
+                010101
+                01010101
+                0101010101
+                010101010101
+                
