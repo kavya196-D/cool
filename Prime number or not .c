5c3aad56188f1eb2fdb86b4aa8fcf09a8a449571
kavya196-D <85491652+kavya196-D@users.noreply.github.com>
2021-08-18 14:37:08 +0530

    Prime number or not.c

--- Prime number or not .c
+++ Prime number or not .c
@@ -34,3 +34,54 @@ int primeno(int num, int i)
        }       
     }
 }
+Output:
+#include <stdio.h>
+ 
+int primeno(int, int);
+ 
+int main()
+{
+    int num, check;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    check = primeno(num, num / 2);
+    if (check == 1)
+    {
+        printf("%d is a prime number\n", num);
+    }
+    else
+    {
+        printf("%d is not a prime number\n", num);
+    }
+    return 0;
+}
+ 
+int primeno(int num, int i)
+{
+    if (i == 1)
+    {
+        return 1;
+    }
+    else
+    {
+       if (num % i == 0)
+       {
+         return 0;
+       }
+       else
+       {
+         return primeno(num, i - 1);
+       }       
+    }
+}
+
+
+OUTPUT:
+
+
+Enter a number: 456
+456 is not a prime number
+ 
+
+
+
