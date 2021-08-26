da9eeef8cfb23ad9ec4e6a656d4dd7e6df946334
kavya196-D <85491652+kavya196-D@users.noreply.github.com>
2021-08-26 16:06:56 +0530

    Bank management system.c

--- Bank management system.c
+++ Bank management system.c
@@ -24,3 +24,28 @@ float interest(float t,float amount,int rate)
     float SI; 
     SI=(rate*t*amount)/100.0;
     return (SI);
+}
+void fordelay(int j)
+{   int i,k; 
+    for(i=0;i<j;i++) 
+    k=i;
+} 
+void new_acc() 
+{ 
+    int choice; 
+    FILE *ptr; 
+    ptr=fopen("record.dat","a+"); 
+    account_no: system("cls"); 
+    printf("\t\t\t\xB2\xB2\xB2\ ADD RECORD \xB2\xB2\xB2\xB2");
+    printf("\n\n\nEnter today's date(mm/dd/yyyy):"); 
+    scanf("%d/%d/%d",&add.deposit.month,&add.deposit.day,&add.deposit.year); 
+    printf("\nEnter the account number:"); 
+    scanf("%d",&check.acc_no); 
+    while(fscanf(ptr,"%d %s %d/%d/%d %d %s %s %lf %s %f %d/%d/%d\n",&add.acc_no,add.name,&add.dob.month,&add.dob.day,&add.dob.year,&add.age,add.address,add.citizenship,&add.phone,add.acc_type,&add.amt,&add.deposit.month,&add.deposit.day,&add.deposit.year)!=EOF) 
+    { 
+        if (check.acc_no==add.acc_no) 
+          {printf("Account no. already in use!"); 
+          fordelay(1000000000); 
+          goto account_no;
+          }
+    }
