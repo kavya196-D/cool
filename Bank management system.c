f11f25fe50125e2d2c199f07d05ba28d83199356
kavya196-D <85491652+kavya196-D@users.noreply.github.com>
2021-08-26 16:11:52 +0530

    Update Bank management system.c

--- Bank management system.c
+++ Bank management system.c
@@ -49,3 +49,38 @@ void new_acc()
           goto account_no;
           }
     }
+    add.acc_no=check.acc_no; 
+        printf("\nEnter the name:"); 
+    scanf("%s",add.name); 
+    printf("\nEnter the date of birth(mm/dd/yyyy):");
+    scanf("%d/%d/%d",&add.dob.month,&add.dob.day,&add.dob.year);
+    printf("\nEnter the age:");
+    scanf("%d",&add.age); 
+    printf("\nEnter the address:"); 
+    scanf("%s",add.address); 
+    printf("\nEnter the citizenship number:");
+    scanf("%s",add.citizenship); 
+    printf("\nEnter the phone number: "); 
+    scanf("%lf",&add.phone); 
+    printf("\nEnter the amount to deposit:$");
+    scanf("%f",&add.amt);
+    printf("\nType of account:\n\t#Saving\n\t#Current\n\t#Fixed1(for 1 year)\n\t#Fixed2(for 2 years)\n\t#Fixed3(for 3 years)\n\n\tEnter your choice:"); 
+    scanf("%s",add.acc_type); 
+        fprintf(ptr,"%d %s %d/%d/%d %d %s %s %lf %s %f %d/%d/%d\n",add.acc_no,add.name,add.dob.month,add.dob.day,add.dob.year,add.age,add.address,add.citizenship,add.phone,add.acc_type,add.amt,add.deposit.month,add.deposit.day,add.deposit.year); 
+    fclose(ptr); 
+    printf("\nAccount created successfully!");
+    add_invalid:
+    printf("\n\n\n\t\tEnter 1 to go to the main menu and 0 to exit:"); 
+    scanf("%d",&main_exit); system("cls");
+    if (main_exit==1) 
+        menu(); 
+    else if(main_exit==0) 
+        close(); 
+    else 
+       {
+          printf("\nInvalid!\a");
+          goto add_invalid; 
+       }
+}
+void view_list()
+{
